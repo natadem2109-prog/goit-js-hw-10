d9ba3e856958b117b7b69c9eb62617a5a9cde068
natadem2109-prog <natadem2109@gmail.com>
2026-03-03 20:44:35 +0000

    Deploying to gh-pages from @ natadem2109-prog/goit-js-hw-10@2c63f7cbe2370e2267f197980c086db490b577eb 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as e}from"./assets/vendor-CZ4EWmTT.js";e("#datetime-picker",{enableTime:!0,dateFormat:"Y-m-d H:i"});const i={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){console.log(t[0])}};e("#datetime-picker",i);
+//# sourceMappingURL=1-timer.js.map
